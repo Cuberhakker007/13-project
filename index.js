76d990b8b76b3b784299b76bd7fd1b044d87bb99
Cuberhakker007 <109462881+Cuberhakker007@users.noreply.github.com>
2023-09-07 21:02:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-
-let del = new Date();
-
-del = del.toString()
-
-let day = del.substr(0,3)
-
-console.log(day);
-
+let del = new Date(); del = del.toString(); let day = del.substr(0,3);
 switch (day) {
-
         case 'Mon': $('#day-1').addClass('bg-color');
         $('#day-1').addClass('col');
-        
         break;
         case 'Tue': $('#day-2').addClass('bg-color');
         $('#day-2').addClass('col'); 
-
         break;
         case 'Wed': $('#day-3').addClass('bg-color');
         $('#day-3').addClass('col');
-
         break;
         case 'Thu': $('#day-4').addClass('bg-color');
         $('#day-4').addClass('col');
-
         break;
         case 'Fri': $('#day-5').addClass('bg-color');
         $('#day-5').addClass('col');
-
         break;
         case 'Sat': $('#day-6').addClass('bg-color');
         $('#day-6').addClass('col');
-
         break;
         case 'Sun': $('#day-7').addClass('bg-color');
         $('#day-7').addClass('col');
-
-
-        break;
-
-    default:
-        break;
-}
+        break; default: break;}
